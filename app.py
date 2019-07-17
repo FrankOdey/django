dec3a167a83a4dad2c8ee4dd6e4211284bea6f00
Frank Odey <frankodey2@gmail.com>
2019-07-17 13:23:57 +0100

    Added the app file

---
+++ app.py
@@ -0,0 +1,38 @@
+def people(students):
+    print(students)
+
+people('Frank')
+# (study) is the argument the function takes.
+# (Frank) is the parameter pass in to the function call as the argument (study)
+
+#EXAMPLE 2
+people('Mary') # Value parameter pass in as argument.
+james = "I'm James"
+people(james) # Variable parameter pass in as argument.
+people('Rita \n'*4) # Expression pass in as argument.
+
+# EXAMPLE 3
+def gameShow(numPlayers):
+    print(str(numPlayers) + ' is the maximum require at this level')
+
+gameShow(500)
+#Outside the function.
+print(str(numPlayers) + ' is not available')
+# numPlayers is not accessible outside the function define because it's the function local variable
+#  and so should be use only in the function body.
+
+#EXAMPLE 4
+def fruit(color):
+    print('Every fruit has a color of ' + color)
+
+fruit('yellow')
+#Outside the function.
+print(yellow + 'is the fruit color')
+
+
+#EXAMPLE 5
+def water(rush_rate, volume):
+    fetch = rush_rate + volume
+    print('this is the ' + str(fetch))
+fetch = 600
+water(50, 30)
